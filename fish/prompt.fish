423ca182f003d0ece47be830fb7a47074d67d14b
soph <heater.hacker.8k@icloud.com>
2025-08-28 21:41:23 +0100

    prompt improvements

--- fish/prompt.fish
+++ fish/prompt.fish
@@ -7,25 +7,14 @@ set DARKPINK (echo -e '\001')(set_color magenta)(echo -e '\002')
 set GREEN (echo -e '\001')(set_color brgreen)(echo -e '\002')
 set DARKGREEN (echo -e '\001')(set_color green)(echo -e '\002')
 set YELLOW (echo -e '\001')(set_color bryellow)(echo -e '\002')
+set DARKYELLOW (echo -e '\001')(set_color yellow)(echo -e '\002')
 set ORANGE (echo -e '\001')(set_color yellow)(echo -e '\002')
 set BLUE (echo -e '\001')(set_color blue)(echo -e '\002')
 set CYAN (echo -e '\001')(set_color brcyan)(echo -e '\002')
 set DARKCYAN (echo -e '\001')(set_color cyan)(echo -e '\002')
 set WHITE (echo -e '\001')(set_color white)(echo -e '\002')
 
-# set -gx MYSQL_PS1 "$PINK"'\u'"$NORMAL"'@'"$GREEN"'\h'"$NORMAL"'['"$DARKPINK"'mysql'"$NORMAL"'] <'"$CYAN"'\T'"$YELLOW"'\d>'"$NORMAL"' mysql> '
-
-set GREP (command -v grep)
-
-# use grep for pcre as rg won't offer any performance improvement for that anyway
-set PGREP (command -v pcre2grep)
-if [ -z "$PGREP" ]
-	set PGREP "$GREP --perl-regexp"
-end
-
-if [ -n (command -v rg) ]
-	set GREP (command -v rg)
-end
+set -gx MYSQL_PS1 "$(printf '\001\033]133;A;\007\002%s:%s>\001\033]133;B;\007\002 ' "$(hostname)" '\d')"
 
 function uncolour_str -a str
 	echo -n "$str" | sed -r 's/\x1B\[[0-9;]*[JKmsu]//g'
@@ -46,75 +35,79 @@ function git_infostr
 		 return
 	end
 
-	set -l gitstr ""
+	set -l stat     (git status 2>&1)
+	set -l branch   (string match -rg 'On branch (.+)'                            $stat)
+	set -l upstream (string match -rg 'Your branch is up to date with \'(.+)\'\.' $stat)
+	set -l head     (test -z "$branch" && echo "$branch" || string match -rg 'HEAD detached at (.+)' $stat)
 
-	set -l BRANCH (git branch --show-current)
-
-	if [ -n "$BRANCH" ];
-		set -l gitstatus (git status 2>&1 | tee)
-		set -l cherry (git cherry $BRANCH 'origin/HEAD' 2>&1 | tee)
-
-		set -l dirty      (echo "$gitstatus" | eval "env $GREP -F 'modified:'                     " > /dev/null 2>&1; echo "$status")
-		set -l unstaged   (echo "$gitstatus" | eval "env $GREP -F 'Changes not staged for commit:'" > /dev/null 2>&1; echo "$status")
-		set -l staged     (echo "$gitstatus" | eval "env $GREP -F 'Changes to be committed:'      " > /dev/null 2>&1; echo "$status")
-		set -l untracked  (echo "$gitstatus" | eval "env $GREP -F 'Untracked files'               " > /dev/null 2>&1; echo "$status")
-		set -l ahead      (echo "$gitstatus" | eval "env $GREP -F 'Your branch is ahead of'       " > /dev/null 2>&1; echo "$status")
-		set -l behind     (echo "$gitstatus" | eval "env $GREP -F 'Your branch is behind'         " > /dev/null 2>&1; echo "$status")
-
-		set -l mixed (echo "$gitstatus" | eval "env $GREP '(This branch is \d+ commits? ahead and \d+ commits? behind)'" > /dev/null 2>&1; echo "$status")
-
-		set -l behind_origin (echo "$cherry" | eval "env $GREP -F '+'" > /dev/null 2>&1; echo "$status")
-
-		set --local UPSTREAM ""
-		set --local dirty_colour ""
-		set --local dirty_symbol ""
-
-		if [ "$ahead"         = "0" ]; set UPSTREAM "$GREEN^";               end
-		if [ "$behind"        = "0" ]; set UPSTREAM "$REDv";                 end
-		if [ "$mixed"         = "0" ]; set UPSTREAM "$YELLOW~";              end
-		if [ "$behind_origin" = "0" ]; set UPSTREAM "$CYAN"["$RED"!"$CYAN"]; end
-		if [ "$staged"        = "0" ]; set dirty_colour "$GREEN";            end
-
-		if [ "$unstaged"  = "0" ] || [ "$untracked" = "0" ];
-			set dirty_colour "$RED"
-			if [ "$staged" = "0" ]; set dirty_colour "$YELLOW"; end
-		end
-
-		if [ "$dirty" = "0" ]; set dirty_symbol '*'; end
-
-		set -l diff (git diff --shortstat)
-
-		set -l inserted (echo "$diff" | eval "env $PGREP -o '\d+(?=\ insertions)'" 2> /dev/null)
-		set -l deleted  (echo "$diff" | eval "env $PGREP -o '\d+(?=\ deletions)'"  2> /dev/null)
-
-		set -l shortstatus (git status --short)
-		# set -f shortstatus (git status --short)
-
-		# Use sed to strip leading whitespace because solaris wc is stupid and adds leading whitespace
-		set -l untracked (echo "$shortstatus" | eval "env $PGREP -o '^\?\?'" 2> /dev/null | wc -l | sed 's/^ *//')
-		set -l added     (echo "$shortstatus" | eval "env $PGREP -o '^\ ?A'" 2> /dev/null | wc -l | sed 's/^ *//')
-		set -l modified  (echo "$shortstatus" | eval "env $PGREP -o '^\ ?M'" 2> /dev/null | wc -l | sed 's/^ *//')
-		set -l delfiles  (echo "$shortstatus" | eval "env $PGREP -o '^\ ?D'" 2> /dev/null | wc -l | sed 's/^ *//')
-
-		set -l newf "$DARKGREEN$added"
-		set -l untr "$DARKCYAN$untracked"
-		set -l delf "$RED$delfiles"
-		set -l mod  "$ORANGE$modified"
-
-		set -l diffstr "$newf$WHITE•$mod$WHITE•$delf$WHITE•$untr"
-
-		if [ -n "$inserted" ] || [ -n "$deleted" ]; set diffstr "$diffstr ";                end
-		if [ -n "$inserted" ];                      set diffstr "$diffstr$GREEN$inserted+"; end
-		if [ -n "$deleted"  ];                      set diffstr "$diffstr$RED$deleted-";    end
-
-		set -l BRANCH_NAME "$CYAN$BRANCH"
-		set -l DIRTY "$dirty_colour$dirty_symbol"
-		set -l DIFFS "$diffstr"
-
-		set gitstr "$WHITE""g<$UPSTREAM$BRANCH_NAME$DIRTY $DIFFS$WHITE>"
+	set -l headstr ""
+	if test "$rpoc_is_refreshing" = "1" 2>/dev/null
+		set -l hash (git rev-parse --short HEAD)
+		set headstr "$WHITE:$DARKCYAN$hash"
 	end
 
-	echo "$gitstr"
+	set -l upstreamstr ""
+	if [ -n "$branch" ];
+		set headstr "$CYAN$branch$headstr"
+		if test "$rpoc_is_refreshing" != "1" 2>/dev/null
+			if string match 'Your branch is ahead of' $stat;
+				set upstreamstr "$GREEN^"
+			else if string match 'Your branch is behind' $stat;
+				set upstreamstr "$RED""v"
+			else if string match -r '(This branch is \d+ commits? ahead and \d+ commits? behind)' $stat;
+				set upstreamstr "$YELLOW~"
+			end
+		end
+
+		# if git cherry "$branch" 'origin/HEAD' | string match '+';
+		# 	set upstream_info "$upstream_info$CYAN[$RED!$CYAN]"
+		# end
+
+		# if [ -n "$upstream" ];
+		# 	set -l cherry (git cherry "$branch" "$upstream" 2>&1)
+		# end
+	else
+		set headstr "$RED""DETACHED$headstr"
+	end
+
+	set -l shortstat (git status --short 2>&1)
+
+	set -l dirtystr ""
+	if string match 'modified:' $stat;
+		set -l unstaged  (string match 'Changes not staged for commit:' $stat)
+		set -l staged    (string match 'Changes to be committed:'       $stat)
+		set -l untracked (string match 'Untracked files'                $stat)
+
+		if [ -n "$staged" ] && [ -n "$unstaged" ] || [ -n "$untracked" ];
+			set dirtystr "$YELLOW*"
+		else if [ -n "$staged" ];
+			set dirtystr "$GREEN*"
+		else if [ -n "$unstaged" ] || [ -n "$untracked" ];
+			set dirtystr "$RED*"
+		end
+	end
+
+	set -l diffstr ""
+	if test "$rpoc_is_refreshing" != "1" 2>/dev/null
+		set -l diff (git diff --shortstat)
+		set -l inserted (string match -rg '(\d+)(?=\ insertions)' $diff)
+		set -l deleted (string match -rg '(\d+)(?=\ deletions)' $diff)
+
+		set -l untracked_count (string match -r '^\?\?' $shortstat | count)
+		set -l added_count     (string match -r '^\ ?A' $shortstat | count)
+		set -l modified_count  (string match -r '^\ ?M' $shortstat | count)
+		set -l deleted_count   (string match -r '^\ ?D' $shortstat | count)
+
+		set diffstr " $DARKGREEN$untracked_count$WHITE•$ORANGE$modified_count$WHITE•$RED$deleted_count$WHITE•$DARKCYAN$untracked_count"
+
+		if [ -n "$inserted" ] || [ -n "$deleted" ];
+			set diffstr "$diffstr ";
+			if [ -n "$inserted" ]; set diffstr "$diffstr$GREEN$inserted+"; end
+			if [ -n "$deleted"  ]; set diffstr "$diffstr$RED$deleted-";    end
+		end
+	end
+
+	echo "$WHITE""g<$upstreamstr$headstr$dirtystr$diffstr$WHITE>"
 end
 
 function git_infostr_loading_indicator -a last
@@ -177,37 +170,43 @@ function custom_prompt_pwd
 	end
 end
 
-set -U async_prompt_functions git_infostr
-
 function time_str
 	if test "$rpoc_is_refreshing" = "1" 2>/dev/null
 		set -l time (date +%H:%M:%S)
-		echo -n (set_color normal)'('$time') '
+		echo -n (set_color normal)'('"$time"')'
 	else
-		echo -n (set_color normal)""
+		echo -n (set_color normal)"(--:--:--) "
 	end
 end
+
+function time_str_loading_indicator -a last
+	echo -n (set_color normal)"(--:--:--) "
+end
+
+set -U async_prompt_functions fish_prompt
+# set -U async_prompt_functions git_infostr
+# shuts up an error in fish-refresh-prompt-on-cmd
+# functions -c fish_prompt '__async_prompt_orig_fish_prompt'
+# set -U async_prompt_functions git_infostr time_str
+# set -U async_prompt_functions fish_prompt
 
 function prompt_str
 	set -l prompt '⋊> '
 
 	if test "$rpoc_is_refreshing" = "1" 2>/dev/null
-		echo -e (set_color normal)"$prompt"(set_color normal)(printf "\033]133;B\007")
+		echo -e (set_color normal)"$prompt"(set_color normal)(printf "\033]133;B;\007")
 	else
-		# echo -e (set_color cyan)"$prompt"(set_color normal)
 		echo -e (set_color normal)"$prompt"(set_color normal)
 	end
 end
 
 function fish_prompt
-	# printf '%s\n%s' (hoststr)(scmstr)" $YELLOW"(custom_prompt_pwd)(set_color normal) '⋊> '
-	
-	echo -e (hoststr)(scmstr)" $YELLOW"(custom_prompt_pwd)"\n"(time_str)(prompt_str)
-end
-
-# function fish_prompt_loading_indicator
-# 	printf '%s\n%s' (hoststr)" $YELLOW"(custom_prompt_pwd)(set_color normal) '⋊> '
-# end
+	if test "$rpoc_is_refreshing" = "1" 2>/dev/null
+		echo -e (hoststr)(scmstr)" $DARKYELLOW"(pwd)"\n"(time_str)" "(prompt_str)
+	else
+		echo -e (hoststr)(scmstr)" $YELLOW"(custom_prompt_pwd)"\n"(prompt_str)
+	end
+ end
 
 function fish_mode_prompt
 	printf ""
@@ -237,14 +236,12 @@ function fish_right_prompt
 			echo $mode
 		end
 	end
-
-	# if test "$rpoc_is_refreshing" = "1" 2>/dev/null
-	# 	set -l time (date +%H:%M)
-	# 	echo " $GRAY$time"
-	# end
 end
 
 function cmd_duration_postexec --on-event fish_postexec
+	set -l cmd_status "$status"
+
+	set -l dur_str ""
 	if test $CMD_DURATION
 		set -l ms "$CMD_DURATION"
 		set -l s  (math "$ms" / 1000)
@@ -253,17 +250,20 @@ function cmd_duration_postexec --on-event fish_postexec
 
 		set -l dur ""
 
-		# Goal: always show around 3 digits of accuracy
+		# always show around 3 digits of accuracy
 		if      [ "$h"  -gt "1"  ]; set dur (printf '%.0f' "$h")h(printf '%.0f' "$m")m
 		else if [ "$m"  -gt "1"  ]; set dur (printf '%.0f' "$m")m(printf '%.0f' "$s")s
 		else if [ "$s"  -gt "1"  ]; set dur (printf '%.2f' "$s")s
 		else;                       set dur "$ms"ms
 		end
 
-		set -l dur_str "$GRAY$dur"
-
-		# echo -n (printf "%-"(math $COLUMNS - (echo "$dur_str" | wc -c))"s%s↵" " " "$dur_str")
-		echo -n "↪$dur_str"
+		set dur_str '('"$GRAY$dur"')'
 	end
-end
 
+	# set -l exitcode_str ""
+	# if [ "$cmd_status" != "0" ];
+	# 	set exitcode_str "- exited with status code: $cmd_status"
+	# end
+
+	echo -ne " ~> $dur_str"
+end
